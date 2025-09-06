63ee53ad09b1b778d3a76efa791af54da4dda3c4
Marcos <exegeses@gmail.com>
2025-09-06 13:11:01 -0300

    variables y constantes en javascript

---
+++ clase1/js/variables.js
@@ -0,0 +1,27 @@
+// declaración -> Hacer que exista una variable
+let puntaje = 600
+let precio = 70.55
+let direccion = 'Lavalle 648, piso 8'
+const PI = 3.141592
+const nacimiento = '05/09/2025'
+
+// console.log()  Sirve para mostrar en la Consola de JavaScript algún dato
+// typeoF() Sirve para saber el tipo de dato
+console.log('puntaje:',puntaje)
+console.log( typeof(puntaje) )
+console.log('precio:',precio)
+console.log( typeof(precio) )
+console.log(direccion)
+console.log( typeof(direccion) )
+
+console.log(PI)
+console.log( typeof(PI) )
+
+
+// Modificamos el valor almacenado en una variable
+precio = 65
+console.log('puntaje:',puntaje)
+console.log('precio:',precio)
+
+// No se puede sobre escribir una constante
+PI = 9
